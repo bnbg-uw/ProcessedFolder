67e134d829a6fdaab3a48b8c5262ea538744dbd4
jontkane@uw.edu <jontkane@uw.edu>
2026-01-08 13:31:45 -0800

    Removed extraneous warning messages

--- src/ReadProcessedFolder.hpp
+++ src/ReadProcessedFolder.hpp
@@ -13,22 +13,19 @@ namespace processedfolder {
 			std::unique_ptr<ProcessedFolder> out = std::make_unique<FusionFolder>(folder);
 			return out;
 		}
-		catch (std::invalid_argument e) { std::cerr << "not a valid fusion folder arg\n"; }
-		catch (lapis::InvalidVectorFileException e) { std::cerr << "not a valid vector file - fusion\n"; }
+		catch (...) {}
 
 		try {
 			std::unique_ptr<ProcessedFolder> out = std::make_unique<LapisFolder>(folder);
 			return out;
 		}
-		catch (std::invalid_argument e) { std::cerr << "cannot interpret this as a lapis folder\n"; }
-		catch (lapis::InvalidVectorFileException e) { std::cerr << "not a valid vector file - lapis\n"; }
+		catch (...) {}
 
 		try {
 			std::unique_ptr<ProcessedFolder> out = std::make_unique<LidRFolder>(folder);
 			return out;
 		}
-		catch (std::invalid_argument e) { std::cerr << "cannot interpret this as a LidR folder\n"; }
-		catch (lapis::InvalidVectorFileException e) { std::cerr << "not a valid vector file - lidr\n"; }
+		catch (...) {}
 
 		return std::unique_ptr<ProcessedFolder>();
 	}
